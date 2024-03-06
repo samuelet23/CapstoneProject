957bf50e0e46b281b1af93151e0ac9a2ac4f5b57
samuelet23 <samueletoscano@hotmail.com>
2024-03-06 17:19:38 +0100

    gameDto with service done

--- back-end/capstone back-end/src/main/java/it/epicode/capstone/Models/DTO/UserDTO.java
+++ back-end/capstone back-end/src/main/java/it/epicode/capstone/Models/DTO/UserDTO.java
@@ -3,21 +3,26 @@ package it.epicode.capstone.Models.DTO;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record UserDTO (
         @NotBlank(message = "The name field cannot be empty/null")
+        @Size(min = 3, message = "Name must be of 3 characters")
         String name,
         @NotBlank(message = "The surname field cannot be empty/null")
+        @Size(min = 3,message = "Surname must be of 3 characters")
         String surname,
         @NotBlank(message = "The username field cannot be empty/null")
+        @Size(min = 5, message = "Username must be of 5 characters")
         String username,
         @NotBlank(message = "The date Of Birth field cannot be empty/null, the date must be dd/MM/yyyy")
         String dateOfBirth,
         @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "not valid email")
         String email,
         @NotBlank(message = "The password field cannot be empty/null")
+        @Size(min = 7, message = "Password must be of 7 characters")
         String password,
         @NotBlank(message = "The confirm password field cannot be empty/null")
         String confirmPassword
--- back-end/capstone back-end/src/main/java/it/epicode/capstone/Models/DTO/UserUpdateDTO.java
+++ back-end/capstone back-end/src/main/java/it/epicode/capstone/Models/DTO/UserUpdateDTO.java
@@ -2,15 +2,19 @@ package it.epicode.capstone.Models.DTO;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserUpdateDTO(
         @NotBlank(message = "The username field cannot be empty/null")
+        @Size(min = 5, message = "Username must be of 5 characters")
         String username,
-        @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "not valid email")
+        @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Not Valid Email")
         String email,
         @NotBlank(message = "The name field cannot be empty/null")
+        @Size(min = 3, message = "Name must be of 3 characters")
         String name,
         @NotBlank(message = "The surname field cannot be empty/null")
+        @Size(min = 3,message = "Surname must be of 3 characters")
         String surname
 ) {
 }
